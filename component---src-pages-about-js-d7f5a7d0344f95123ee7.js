59c3eda6531b88bde6347fe513ef12fa970eda8f
Juan Gama <juangama@192.168.0.5>
2021-01-30 16:56:39 -0500

    Updates

---
+++ component---src-pages-about-js-d7f5a7d0344f95123ee7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"3XHS":function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return i}));n("q1tI");var o=n("Bl7J"),a=n("AeFk");function i(t){var e=t.data;return Object(a.b)(o.a,null,Object(a.b)("h1",null,e.site.siteMetadata.title),Object(a.b)("p",null,"We're the only site running on your computer dedicated to showing the best photos and videos of pandas eating lots of food."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-d7f5a7d0344f95123ee7.js.map
